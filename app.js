e6dde1bcd68324cd6e9f86cc2a02e25295a2df86
FRz3ro <felipe.rojas.dev04@gmail.com>
2025-08-08 17:27:32 -0400

    Prueba de branch y Comentarios de funciones a realizar

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Crear un Array para almacenar nombres
+// Implementar una funcion para agregar amigos
+// Implementar una funcion para actualizar la lista de amigos
+// Implementar una funcion para sortear los amigos
